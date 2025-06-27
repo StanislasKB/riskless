efff438c758876b7c5daa0f0574dd77e7c7a2cc7
bienvenudev <bienvenuacclombessi8@gmail.com>
2025-06-28 00:49:59 +0100

    feat: start

---
+++ app/Http/Controllers/PlanActionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlanActionController extends Controller
+{
+    //
+    public function index(){
+
+
+
+        return view('plan_action.index');
+
+    }
+}
