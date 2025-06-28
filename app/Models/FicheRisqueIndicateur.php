e35426d37879946a3e823046222b2d4b66d113d8
Stanislas <stanislasbayord79@gmail.com>
2025-06-28 09:50:21 +0100

    service manager

--- app/Models/FicheRisqueIndicateur.php
+++ app/Models/FicheRisqueIndicateur.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class FicheRisqueIndicateur extends Model
 {
-    //
+    protected $fillable = [
+        'fiche_risque_id',
+        'indicateur_id',
+    ];
 }
--- app/Models/FicheRisquePlanAction.php
+++ app/Models/FicheRisquePlanAction.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class FicheRisquePlanAction extends Model
 {
-    //
+    protected $fillable = [
+        'fiche_risque_id',
+        'plan_action_id',
+    ];
 }
