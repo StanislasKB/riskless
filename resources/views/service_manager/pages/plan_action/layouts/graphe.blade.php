cdc5f9d1224e908436d0f948cb248d34ee54aecb
bienvenudev <bienvenuacclombessi8@gmail.com>
2025-07-03 01:23:14 +0100

    fix:add graph

---
+++ resources/views/service_manager/pages/plan_action/layouts/graphe.blade.php
@@ -0,0 +1,12 @@
+{{-- Graphe ici --}}
+<div class="card radius-15">
+    <div class="card-body">
+        <div class="card-title">
+            <h4 class="mb-0">Chart of fiche</h4>
+        </div>
+        <hr />
+        <div class="chart-container1">
+            <canvas id="riskMatrixChart"></canvas>
+        </div>
+    </div>
+</div>
